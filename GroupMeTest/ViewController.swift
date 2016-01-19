3bae51ad5a710e5fe12641e21ea7431238b5ec31
Briguy52 <Briguy52@gmail.com>
2016-01-19 13:12:31 -0500

    Added print statements to debug redirect

--- GroupMeTest/ViewController.swift
+++ GroupMeTest/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startToDashSegue" {
             let dashVC = segue.destinationViewController as! DashboardTableViewController
+            print("segue received") 
         }
     }
 
